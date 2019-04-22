041be8ffed3af9cdfbb06f32e676d9108d748094
guduchengji <46864595+guduchengji@users.noreply.github.com>
2019-04-22 18:12:59 +0800

    更新了入口类的描述
    补充了入口类的包扫描

--- src/main/java/com/xlx/GithubEmsApplication.java
+++ src/main/java/com/xlx/GithubEmsApplication.java
@@ -2,12 +2,14 @@ package com.xlx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.xlx.dao")
 public class GithubEmsApplication {
     //这是一个入口类
     public static void main(String[] args) {
+        system.out.println("博哥走的第一个半月，想他");
         SpringApplication.run(GithubEmsApplication.class, args);
     }
-
 }
