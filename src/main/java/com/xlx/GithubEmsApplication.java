1a34f576f09421a01427ec2abe122e0515c117b9
xlx <guduchengji@sina.com>
2019-04-22 18:23:04 +0800

    update join class

--- src/main/java/com/xlx/GithubEmsApplication.java
+++ src/main/java/com/xlx/GithubEmsApplication.java
@@ -2,11 +2,14 @@ package com.xlx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.xlx.dao")//补充了入口类
 public class GithubEmsApplication {
     //这是一个入口类
     public static void main(String[] args) {
+        System.out.println("真男人就该干变形金刚");
         SpringApplication.run(GithubEmsApplication.class, args);
     }
 
