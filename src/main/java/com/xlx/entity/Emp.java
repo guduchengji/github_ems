77800d4d0e5d2b0443ff79f8d7e52723ffa6659e
xlx <guduchengji@sina.com>
2019-04-22 18:59:36 +0800

    update entity

--- src/main/java/com/xlx/entity/Emp.java
+++ src/main/java/com/xlx/entity/Emp.java
@@ -1,6 +1,18 @@
 package com.xlx.entity;
 
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table
+@Data
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class Emp {
+    @Id
     private String id;
 
     private String name;
@@ -11,43 +23,4 @@ public class Emp {
 
     private Integer salary;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id == null ? null : id.trim();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
-
-    public Integer getSex() {
-        return sex;
-    }
-
-    public void setSex(Integer sex) {
-        this.sex = sex;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Integer getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Integer salary) {
-        this.salary = salary;
-    }
 }
--- src/main/java/com/xlx/entity/User.java
+++ src/main/java/com/xlx/entity/User.java
@@ -1,33 +1,22 @@
 package com.xlx.entity;
 
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table
+@Data
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
     private String id;
 
     private String username;
 
     private String password;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id == null ? null : id.trim();
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username == null ? null : username.trim();
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password == null ? null : password.trim();
-    }
-}
+   }
